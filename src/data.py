639fde0b9538139c47758bddc225eb327a771377
JackieTien97 <JackieTien@foxmail.com>
2020-04-23 16:26:48 +0800

    change structure

--- src/data.py
+++ src/data.py
@@ -10,7 +10,6 @@ def get_data(path, batch_size, bptt_len, device=-1):
                                                                    train="train.txt",
                                                                    validation="valid.txt", text_field=TEXT)
     TEXT.build_vocab(train, max_size=MAX_VOCAB_SIZE)
-    print("vocabulary size:{}".format(len(TEXT.vocab)))
     # 构建train、val的迭代器（list）
     train_iter, val_iter = torchtext.data.BPTTIterator.splits(
         (train, val), batch_size=batch_size, device=device, bptt_len=bptt_len,
--- src/main.py
+++ src/main.py
@@ -8,49 +8,12 @@ from matplotlib import pyplot as plt
 import data
 import model
 
-num_epochs = 200
-batch_size = 16
-bptt_len = 32
-SEED = 12345
-
-print("num_epochs: ", num_epochs)
-print("batch_size: ", batch_size)
-print("bptt_len: ", bptt_len)
-
-# Set the random seed manually for reproducibility.
-torch.manual_seed(SEED)
-print("seed: ", SEED)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("torch.cuda.is_available(): ", torch.cuda.is_available())
-# load data
-train_iter, val_iter, VOCAB_SIZE = data.get_data("../data/ptb/", batch_size, bptt_len, device)
-print("VOCAB_SIZE: ", VOCAB_SIZE)
-
 
 def repackage_hidden(h):
     if isinstance(h, torch.Tensor):
         return h.detach()
     else:
         return tuple(repackage_hidden(v) for v in h)
-
-
-# WRITE CODE HERE within two '#' bar
-########################################
-# Build LMModel model (build your language model here)
-embedding_size = 128
-hidden_size = 256
-layer_number = 3
-MyModel = model.LMModel(VOCAB_SIZE, embedding_size, hidden_size, layer_number, 0.7)
-print(MyModel)
-MyModel.to(device)
-########################################
-
-criterion = nn.CrossEntropyLoss()
-learning_rate = 0.001
-step_size = 20
-optimizer = torch.optim.Adam(MyModel.parameters(), lr=learning_rate)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=0.5)
-GRAD_CLIP = 1
 
 
 # WRITE CODE HERE within two '#' bar
@@ -109,37 +72,79 @@ def train(rnn_model, train_data, optim, sched, device):
     return epoch_loss
 
 
-########################################
+if __name__ == '__main__':
 
-save_directory = '../model/'
-train_loss_array = []
-valid_loss_array = []
-best_loss = float('inf')
-# Loop over epochs.
-for epoch in range(1, num_epochs + 1):
-    print('epoch:{:d}/{:d}'.format(epoch, num_epochs))
-    print('*' * 100)
-    train_loss = train(MyModel, train_iter, optimizer, scheduler, device)
-    train_loss_array.append(train_loss)
-    print("training loss: {:.4f}".format(train_loss))
-    valid_loss = evaluate(MyModel, val_iter, device)
-    valid_loss_array.append(valid_loss)
-    print("validation perplexity: {:.4f}".format(valid_loss))
-    if valid_loss < best_loss:
-        best_acc = valid_loss
-        best_model = model
-        torch.save(best_model, os.path.join(save_directory, 'best_model.pt'))
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!START!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-print("train_loss_array: ", train_loss_array)
-print("valid_loss_array: ", valid_loss_array)
-print("best validation perplexity is: {:.4f}".format(best_loss))
+    num_epochs = 200
+    batch_size = 16
+    bptt_len = 32
+    SEED = 12345
 
-plt.figure()
-plt.title("Training and Validation Loss vs. Number of Training Epochs")
-plt.xlabel("Training Epochs")
-plt.ylabel("Loss")
-plt.plot(range(1, num_epochs + 1), train_loss_array, label="Training")
-plt.plot(range(1, num_epochs + 1), valid_loss_array, label="Validation")
-plt.xticks(np.arange(1, num_epochs + 1, 20.0))
-plt.legend()
-plt.savefig('Loss.jpg')
+    print("num_epochs: ", num_epochs)
+    print("batch_size: ", batch_size)
+    print("bptt_len: ", bptt_len)
+
+    # Set the random seed manually for reproducibility.
+    torch.manual_seed(SEED)
+    print("seed: ", SEED)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("torch.cuda.is_available(): ", torch.cuda.is_available())
+    # load data
+    train_iter, val_iter, VOCAB_SIZE = data.get_data("../data/ptb/", batch_size, bptt_len, device)
+    print("VOCAB_SIZE: ", VOCAB_SIZE)
+
+    # WRITE CODE HERE within two '#' bar
+    ########################################
+    # Build LMModel model (build your language model here)
+    embedding_size = 128
+    hidden_size = 256
+    layer_number = 3
+    MyModel = model.LMModel(VOCAB_SIZE, embedding_size, hidden_size, layer_number, 0.7)
+    print(MyModel)
+    MyModel.to(device)
+    ########################################
+
+    criterion = nn.CrossEntropyLoss()
+    learning_rate = 0.001
+    step_size = 20
+    optimizer = torch.optim.Adam(MyModel.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=0.5)
+    GRAD_CLIP = 1
+
+    ########################################
+
+    save_directory = '../model/'
+    train_loss_array = []
+    valid_loss_array = []
+    best_loss = float('inf')
+    # Loop over epochs.
+    for epoch in range(1, num_epochs + 1):
+        print('epoch:{:d}/{:d}'.format(epoch, num_epochs))
+        print('*' * 100)
+        train_loss = train(MyModel, train_iter, optimizer, scheduler, device)
+        train_loss_array.append(train_loss)
+        print("training loss: {:.4f}".format(train_loss))
+        valid_loss = evaluate(MyModel, val_iter, device)
+        valid_loss_array.append(valid_loss)
+        print("validation perplexity: {:.4f}".format(valid_loss))
+        if valid_loss < best_loss:
+            best_acc = valid_loss
+            best_model = model
+            torch.save(best_model, os.path.join(save_directory, 'best_model.pt'))
+
+    print("train_loss_array: ", train_loss_array)
+    print("valid_loss_array: ", valid_loss_array)
+    print("best validation perplexity is: {:.4f}".format(best_loss))
+
+    plt.figure()
+    plt.title("Training and Validation Loss vs. Number of Training Epochs")
+    plt.xlabel("Training Epochs")
+    plt.ylabel("Loss")
+    plt.plot(range(1, num_epochs + 1), train_loss_array, label="Training")
+    plt.plot(range(1, num_epochs + 1), valid_loss_array, label="Validation")
+    plt.xticks(np.arange(1, num_epochs + 1, 20.0))
+    plt.legend()
+    plt.savefig('Loss.jpg')
+
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!END!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
