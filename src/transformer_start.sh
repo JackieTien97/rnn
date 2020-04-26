89b5311b322d2a12a42ec2f9e9c102dd01301902
JackieTien97 <JackieTien@foxmail.com>
2020-04-26 11:23:49 +0800

    start code

--- src/transformer_start.sh
+++ src/transformer_start.sh
@@ -1,1 +1,1 @@
-nohup python3 -u transformer_main.py > log_transformer_layer3_bptt64_dropout_0_2.txt 2>&1 &
+nohup python3 -u transformer_main.py > log_bleu_transformer_layer3_bptt64_dropout_0_2.txt 2>&1 &
